95210810bcdc11e3b5b894006e1416e84d1baa79
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-05-25 17:35:49 -0400

    1st commit

---
+++ Array and Hashing/concatenation_of_Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+
+        int[] ans = new int[2*n]; // will take space complexity of O(N)
+
+        // Time complexity = O(N)
+        for (int i = 0; i < n ; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+
+        return ans;
+    }
+}
