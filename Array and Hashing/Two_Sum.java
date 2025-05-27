bf1bac68eda0ead551dfa9389b0863b8a49bca88
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-05-27 00:34:21 -0400

    Optimal Approach using hashmap

--- Array and Hashing/Two_Sum.java
+++ Array and Hashing/Two_Sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
+        /*
         // Brut Force Approach
 
         // TC - O(N^2) used two for loops 
@@ -18,5 +19,33 @@ class Solution {
         }
 
         return ans;
+        */
+
+        // ---------------------------------------------------
+
+        // Optimal Approach
+
+        // TC - O(N)
+        // SC - O(N)
+
+        int[] ans = new int[2];
+
+        Map<Integer, Integer> HM = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if(HM.containsKey(difference)) {
+                int oneIndex = i;
+                int otherIndex = HM.get(difference);
+                ans[0] = oneIndex;
+                ans[1] = otherIndex;
+            }
+            else {
+                HM.put(nums[i], i);
+            }
+        }
+
+        return ans;
+
      }
 }
