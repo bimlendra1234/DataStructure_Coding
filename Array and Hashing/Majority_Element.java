afce67ee105229cd048ad54f03aaaaed85609a19
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-05-28 00:58:57 -0400

    Brut Force approach with optimized Hash Map

--- Array and Hashing/Majority_Element.java
+++ Array and Hashing/Majority_Element.java
@@ -3,7 +3,8 @@ import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
         
-        // Brut Force Approach 
+        /*
+        // Brut Force Approach With Non Optimized Hash MAP
 
         // TC - O(N)
         // SC - O(N)
@@ -16,6 +17,7 @@ class Solution {
 
         // Traverse the HM
         // Get the maximum Value
+
         int maxValue = Integer.MIN_VALUE;
         for (Map.Entry<Integer,Integer> entry : HM.entrySet()) {
             if(entry.getValue() >= maxValue && entry.getValue() > nums.length/2) {
@@ -31,5 +33,52 @@ class Solution {
         }
 
         return -1;
+        */
+
+        // -------------------------------------------------------------------------------------------
+
+        // Brut Force Approach With Optimized Hash MAP
+
+        // TC - O(N)
+        // SC - O(N)
+
+        Map<Integer,Integer> HM = new HashMap<>();
+
+        // get the max count while inserting itself as well
+        // also while doing the same take key value and store it as well with max value.
+        int maxValue = Integer.MIN_VALUE;
+        int majorityElement = nums[0];
+
+
+        for (int i = 0; i < nums.length; i++) {
+            HM.put(nums[i], HM.getOrDefault(nums[i],0)+1);
+
+            if(HM.get(nums[i]) > maxValue && HM.get(nums[i]) > nums.length/2) {
+                maxValue = HM.get(nums[i]); // associated value of key
+                majorityElement = nums[i];  // associated key
+            }
+        }
+
+        return majorityElement;
+
+        // -------------------------------------------------------------------------------------------
+
+        
+        // Optimal Approach
+
+        // TC - O(N)
+        // SC - O(1)
+
+        /*
+        // Boyer-Moore Voting Algorithm - only work if there is confirm majority number
+        
+        1. using two variable count = 0 and value = 0
+        2. Do single traversal
+        3. while looking each number
+        4. on looking each number, if count = 0, change the value to that number and increment the count
+        5. if other number is different just decrement the count by 1
+        6. Finally return the value that is basically the majority number
+        */
+
     }
 }
