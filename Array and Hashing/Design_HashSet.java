a52c3b52c1522b310415e5eec37aa910a5f6cd93
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-05-30 23:20:03 -0400

    Actual Implementation without time complexity unknown

--- Array and Hashing/Design_HashSet.java
+++ Array and Hashing/Design_HashSet.java
@@ -1,10 +1,16 @@
+import java.util.*;
+
 class MyHashSet {
+
+    /*
     // Most basic approach but noot good
+    // TC: O(1)
+    // SC: O(N)
+
+    // taking advantage of the fact that we know 0 <= key <= 10^6
+
     boolean[] arr;
     public MyHashSet() {
-        // Initialize data structure here.
-        // Using boolean array of size 1000001 to store presence of keys
-        // as keys are in the range [0, 1000000].
         arr = new boolean[1000001]; 
     }
     
@@ -18,6 +24,54 @@ class MyHashSet {
     
     public boolean contains(int key) {
         return arr[key];
+    }
+    */
+
+    // ---------------------------
+
+    // Actual Implementation approach that need to be understood
+    // TC: ?
+    // SC: ?
+
+    final int bucketSize = 100;
+    List<List<Integer>> bucket; // declaring the data Structure
+    public MyHashSet() {
+        bucket = new ArrayList<>(bucketSize);  // initializing the data structure
+        for(int i = 0; i < bucketSize; i++) {  // no list inside the bucket now
+            bucket.add(null);
+        }
+    }
+    
+    public void add(int key) {
+        int index = key%bucketSize;
+        List<Integer> childList = bucket.get(index);
+
+        if(childList == null) {
+            List<Integer> newList = new LinkedList<>();
+            newList.add(key);
+            // .add for initializing
+            // .set for updating
+            bucket.set(index, newList); 
+        } else {
+            if(!childList.contains(key)) {
+                childList.add(key);
+            }
+        }
+    }
+    
+    public void remove(int key) {
+        int index = key%bucketSize;
+        List<Integer> childList = bucket.get(index);
+        if(childList != null && childList.contains(key)) {
+            childList.remove(Integer.valueOf(key));
+        }
+     }
+    
+    public boolean contains(int key) {
+        int index = key % bucketSize;
+        List<Integer> childList = bucket.get(index);
+
+        return childList != null && childList.contains(key);
     }
 }
 
