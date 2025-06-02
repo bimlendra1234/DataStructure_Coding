0a78308ee044c794d4e7405feb4b735a124010ee
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-02 11:19:03 -0400

    Optimal approach with counting the number of 0's, 1's and 2's

--- Array and Hashing/Sort_Colors.java
+++ Array and Hashing/Sort_Colors.java
@@ -12,6 +12,7 @@ class Solution {
 
         // ------------------------------------------------------
 
+        /*
         // BF approach
 
         // TC: O(N^2)
@@ -38,6 +39,52 @@ class Solution {
                 nums[i] = nums[minIndex];
                 nums[minIndex] = temp;
             }
+        }
+        */
+
+        // ------------------------------------------------
+
+        // Optimal Approach
+
+        // TC: O(N)
+        // SC: O(1)
+
+        /*
+        // Approach
+        1. use one for loop and in that process
+        2. count number of 0, 1 and 2;
+        3. replace the num array with that much number of 0, then 1 and then 2.
+        */
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for(int i =0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                red++;
+            }
+            else if(nums[i] == 1) {
+                white++;
+            }
+            else {
+                blue++;
+            }
+        }
+
+        // Debugging
+        System.out.println("red: " + red);
+        System.out.println("white: " + white);
+        System.out.println("blue: " + blue);
+        
+        for (int i = 0; i < red; i++) {
+            nums[i] = 0;
+        }
+
+        for (int i =red; i < white+red; i++) {
+            nums[i] = 1;
+        }
+
+        for (int i = white+red; i < white+red+blue; i++) {
+            nums[i] = 2;
         }
     }
 }
