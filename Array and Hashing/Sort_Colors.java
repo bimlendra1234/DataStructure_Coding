b0f420ff0f1798355d9649925525c2937d386d3f
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-01 22:55:08 -0400

    Using inbuilt array sort

--- Array and Hashing/Sort_Colors.java
+++ Array and Hashing/Sort_Colors.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Solution {
     public void sortColors(int[] nums) {
+        // TC: O(n log n)
+        // SC: O(1) if we consider the input array as the output
         Arrays.sort(nums);
     }
 }
