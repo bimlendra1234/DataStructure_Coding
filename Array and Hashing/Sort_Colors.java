577c60237a76c293474daa0abfaeed4e0be2fcc8
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-02 10:51:27 -0400

    BF Approch came out of me

--- Array and Hashing/Sort_Colors.java
+++ Array and Hashing/Sort_Colors.java
@@ -2,8 +2,42 @@ import java.util.*;
 
 class Solution {
     public void sortColors(int[] nums) {
+        
+        /*
+        // Inbuilt Array sorting Approach
         // TC: O(n log n)
         // SC: O(1) if we consider the input array as the output
         Arrays.sort(nums);
+        */
+
+        // ------------------------------------------------------
+
+        // BF approach
+
+        // TC: O(N^2)
+        // SC: O(1)
+
+        // Approach
+        // 1. run for loop and take each element at once
+        // 2. from that element, next to all forwardtill last, find minimam element 
+        // 3. if element considered at step 1 is greater then minimum element
+        // 4. then swap element considered and minimum element
+        
+        for(int i = 0; i < nums.length-1; i++) {
+            int valToReplace = nums[i];
+            int minNumber = Integer.MAX_VALUE;
+            int minIndex = i+1;
+            for(int j = i+1; j < nums.length;j++) {
+                if(nums[j] <= minNumber) {
+                    minNumber = nums[j];
+                    minIndex = j;
+                }
+            }
+            if(nums[i]>nums[minIndex]) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
     }
 }
