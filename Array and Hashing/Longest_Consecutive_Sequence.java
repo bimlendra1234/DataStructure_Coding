1b0b7bdd16306e441ed37e5ef6fc11614f0299a8
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-06 18:27:28 -0400

    Optimal Approach using Hashmap , number and seen

--- Array and Hashing/Longest_Consecutive_Sequence.java
+++ Array and Hashing/Longest_Consecutive_Sequence.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
 
+        /*
         // BrutForce Approach
         // with sorting and and for each element calculating its longest common sequences and taking maximum one
 
@@ -42,5 +43,58 @@ class Solution {
             LongestCommonSeq = Math.max(LongestCommonSeq, maxLongSeq);
         }
         return LongestCommonSeq;
+        */
+
+        // --------------------------------------------
+
+        // Optimal Approach using HashMap
+
+        // TC: O(N)
+        // SC: O(N)
+
+        /*
+        Approach:
+        1. Create Hashmap and inilitiaze all elem to false to denote not travelled
+        2. for each element check forward consecutive element
+            if next num is available in HM and is false
+            Increment len count and set target next number
+        3. for same element check backward consecutive element as well
+            if prev num is available in HM and is false then make it true
+            Increment len count and set target prev number number
+        4. return maximum length among all above consecutive length
+        */
+
+        int maxConsecutiveLength = 0;
+        Map<Integer, Boolean> HM = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            HM.put(nums[i], false);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int eachElemConsecutiveCount = 1;
+
+            // Finding Forward Consecutive
+            int currentNum = nums[i];
+            int nextNum = currentNum + 1;
+            while(HM.containsKey(nextNum) && HM.get(nextNum) == false) { // false means not seen
+                eachElemConsecutiveCount++;
+                HM.put(nextNum, true);
+                nextNum++;
+            }
+
+            // Finding Backward Consecutive
+            int prevNum = currentNum - 1;
+            while(HM.containsKey(prevNum) && HM.get(prevNum) == false) { // false means not seen
+                eachElemConsecutiveCount++;
+                HM.put(prevNum, true);
+                prevNum--;
+            }
+
+            maxConsecutiveLength = Math.max(maxConsecutiveLength, eachElemConsecutiveCount);
+        }
+
+        return maxConsecutiveLength;
     }
 }
