099c20763e712033d3a84f10ad110cba178fa903
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-08 15:23:55 -0400

    Optimal Approach using Boyer-Moore Voting Algorithm extended using two pass. one for two majority element and other for verifying weather it is greater then n/3

--- Array and Hashing/Majority_Element_II.java
+++ Array and Hashing/Majority_Element_II.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
+        /*
         // BrutForce Approach using HAshMap
 
         // TC: O(N)
@@ -35,6 +36,78 @@ class Solution {
                 res.add(key);
             }
         }
+
+        return res;
+        */
+
+        // -------------------------------------------------------------------------------------------------------
+
+        // Optimal Approach using Boyer-Moore Voting Algorithm extended
+
+        // TC: O(N)
+        // SC: O(1)
+
+        /*
+        Approach
+        1. find most two frequent element using Boyer-Moore Voting Algorithm
+        2. Do the two pass of actual array
+        3. the first pass of the Boyer-Moore Voting Algorithm only finds candidates that might be majority elements
+            not necessirily greater then 3
+        4. The Second pass verify the two majority element count that it is greater then n/3
+            does a literal count of the candidate values in the array
+            later this number is only applied when this count is greater then n/3
+        5. create resultant arraylist and add this two number in it
+            if its count is greater then n/3
+        */
+
+        /*
+        Doubt
+        How we can extend Boyer-Moore Voting Algorithm to find two majority element
+        because initially we only found one majority element
+        */
+
+        int count1 = 0;
+        int majorityElement1 = 0;
+        int count2 = 0;
+        int majorityElement2 = 0;
+
+        // find most two frequent element
+        // the first pass of the Boyer-Moore Voting Algorithm only finds candidates that might be majority elements
+        for (int i = 0; i < nums.length; i++) {
+            if (majorityElement1 == nums[i]) {
+                count1++;
+            }
+            else if (majorityElement2 == nums[i]) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                    majorityElement1 = nums[i];
+                    count1++;
+            }
+            else if (count2 == 0) {
+                    majorityElement2 = nums[i];
+                    count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // verify the two majority element count
+        // This loop does a literal count of the candidate values in the array
+        // later this number is only applied when this count is greater then n/3
+        count1 = 0;
+        count2 = 0;
+        for(int num: nums) {
+            if(num == majorityElement1) count1++;
+            else if(num == majorityElement2) count2++;
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        // only applied when this count is greater then n/3
+        if(count1>nums.length/3) res.add(majorityElement1);
+        if(count2>nums.length/3) res.add(majorityElement2);
 
         return res;
     }
