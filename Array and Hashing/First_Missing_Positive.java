8b9af81c0941622575005b59da82cac1b157fe15
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-09 16:15:38 -0400

    Approach using HashSet with big oh of n time complexity

--- Array and Hashing/First_Missing_Positive.java
+++ Array and Hashing/First_Missing_Positive.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
 
+        /*
         // BrutForse Approach with sorting technique 
 
         // TC: O(NlogN) for sorting
@@ -19,5 +20,41 @@ class Solution {
         }
 
         return missing;
+        */
+
+        // ----------------------------------------------------
+
+        // Approach using HashSet
+
+        // TC: O(N)
+        // SC: O(N)
+        
+        // Approach
+        // 1. create a HashSet to store all positive numbers
+        // 2. iterate through the array and add all positive numbers to the set
+        // 3. initialize a variable missing to 1 (the first positive number)
+        // 4. while missing is in the set, increment missing by 1
+        // 5. return missing
+
+        // Doubt in iterating hash map - similar to array : // HS.contains(missing)
+        // or for (int num : HS) {
+
+        Set<Integer> HS = new HashSet<>();
+
+        for(int num : nums) {
+            if(num > 0) {
+                HS.add(num);
+            }
+        }
+
+        int missing = 1;
+        for (int num : HS) {
+            if(num == missing){
+                missing++;
+            }
+        }
+
+        return missing;
+
      }
 }
