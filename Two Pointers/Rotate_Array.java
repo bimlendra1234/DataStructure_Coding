4261c4f1971f318310a1bfa5803bc852975396ac
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-14 15:13:45 -0400

    Optimal Approach Using reverse and two pointer

--- Two Pointers/Rotate_Array.java
+++ Two Pointers/Rotate_Array.java
@@ -1,5 +1,7 @@
 // Leetcode: 189. Rotate Array
 
+/*
+// BrutForce Approach using extra array space
 class Solution {
     public void rotate(int[] nums, int k) {
 
@@ -44,5 +46,63 @@ class Solution {
         for(int i = 0; i < temp.length; i++) {
             nums[i] = temp[i];
         }
+    }
+}
+*/
+
+// ----------------------------------------------------
+
+// Optimal Approach Using reverse and two pointer
+
+class Solution {
+
+    // Extra Function that I created
+    public void reverseArray(int left, int right, int[] nums){
+        while(left<right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+
+        // Optimal Approach Using reverse and two pointer
+        // TC: O(N)
+        // SC: O(1)
+
+
+        // *********
+        // Approach
+        /*
+        let say array is [1,2,3,4,5,6,7] and k = 3 (i.e shift each element by two time)
+        all reversal logic in the same array nums
+
+        1. reverse entire array so, 
+            [1,2,3,4,5,6,7] => [7,6,5,4,3,2,1]
+        2. reverse upto k (if index start from 0 so k-1) array (i.e 0 to k-1) so, 
+            [7,6,5] => [5,6,7]
+        3. reverse remaining k to last element (i.e nums.length-1) so, 
+            [4,3,2,1] => [1,2,3,4]
+        4. so the final array nums will be
+            [5,6,7,1,2,3,4]
+        */
+
+
+        // *********
+        // Code
+
+        // if k is greater the array length that means it will reverse in same array multiple time.
+        k = k%nums.length; // let say k =2
+
+        // reverse the entire nums: 1,2,3,4,5 => 5,4,3,2,1
+        reverseArray(0, nums.length-1, nums);
+
+        // reverse first k element : 5,4 => 4,5
+        // also reverse k to all element: 3,2,1 => 1,2,3
+        reverseArray(0, k-1, nums);
+        reverseArray(k, nums.length-1, nums);
     }
 }
