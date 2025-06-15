a74630d9da90b795d6c0bb874d960fd42e19d05f
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-15 00:44:31 -0400

    Optimal Approach using two pointer method

--- Two Pointers/Container_With_Most_Water.java
+++ Two Pointers/Container_With_Most_Water.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int maxArea(int[] height) {
-
+        /*
         // BrutForce Approach 
             // by calculating max area of each verticle line with other and taking max among all
         // TC: O(N^2)
@@ -33,5 +33,77 @@ class Solution {
         }
 
         return maxArea;
+        */
+
+        // ----------------------------------------------------------------
+        
+        // Optimal Approach using two pointer method
+        // TC: O(N)
+        // SC: O(1)
+
+
+        // *********
+        // Approach
+        /*
+        1. Maintain two pointer left at 0 and right at last element
+        2. calculte area between these two
+            set initial area to min value, along the process store the max area 
+        3. now for shifting the pointer
+            if left < right, do left++
+            if right <= left, do right--
+        4. finally return max area, that we saved long all the area.
+        */
+
+
+        // *********
+        // Doubt
+        /*
+        1. why only till left<right?
+
+            Area = min(height[left], height[right]) × (right - left)
+            So, I always do
+                Keep width high (initially large)
+                and Increase min height by moving the shorter side inward
+
+            This strategy ensures that every better area that could possibly exist is considered, 
+            and nothing is missed.
+
+        ------------------------------------------------------------------------------
+        2. how to understand when to do left++ and right--, i.e shifting the pointer.?
+
+             Always move the pointer at the shorter height, 
+             because only that gives a chance of increasing minHeight 
+                (the limiting factor for area) i.e MinHeight * base.
+
+            Moving the larger pointer doesn't help because the shorter height limits the container.
+
+            When height[left] < height[right] => do left++ 
+                Because left is the bottleneck; move it to possibly get a taller line
+
+            When height[left] >= height[right] => do right-- 
+                Because right is the bottleneck; move it to possibly get a taller line
+        */
+
+
+        // *********
+        // Code
+        int left = 0;
+        int right = height.length-1;
+
+        int area = Integer.MIN_VALUE;
+        while(left < right) {
+            int min = Math.min(height[left], height[right]);
+            int base = right-left;
+            area = Math.max(area, min*base);
+            
+            if(height[left]< height[right]) {
+                left++;
+            }
+            else {
+                // in case if height[left]>= height[right]
+                right--;
+            }
+        }
+        return area;
     }
 }
