2d4c140ca815c40192f9f428a0c777d51c8003f9
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-21 14:41:43 -0400

    Approach Using One Queue

--- Stack/3. Implement Stack using Queues.java
+++ Stack/3. Implement Stack using Queues.java
@@ -3,12 +3,74 @@
 import java.util.*;
 
 
+/*
+// Approach Using One Two queue - other key mainly used while popping
+// TC of push : O(N)
+// SC of Push: O(N) for using one extra queue
+// TC of rest : O(1)
+// SC of rest : O(1)
+
+
+// *********
+// Approach
+// 1. we need to simulate the operation in queue such that outcome is same as stack using two queue while push
+// 2. insert in queue such a way that 
+    // outcome in both stack and queue is similar
+    // then already same for remove, peek and isEmpty - no need to wory
+// 3. Only push in a way that
+        // add the element in the queue2
+        // untill queue is empty pop abd push all its element to queue2
+        // finally swap queue1 and queue2
+
+
+// *********
+// Code
+class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        // here is only the difference, insert in such a way that it is equivalent to stack
+        queue2.offer(x);
+
+        // while q1 is not entire empty
+        // pop and add in the queue2
+        while(queue1.size()!=0) {
+            int elem = queue1.poll();
+            queue2.offer(elem);
+        }
+
+        // swap queue1 and queue2. i.e queue2 become queue1 now
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+     public int pop() {
+        return queue1.poll();
+    }
+    
+    public int top() {
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+*/
+
 // ------------------------------------------------------------------------------------
 
 // Approach Using One Queue
 // TC of Push : O(N)
 // TC of rest : O(1)
 // SC: O(1)
+
 
 // *********
 // Approach
