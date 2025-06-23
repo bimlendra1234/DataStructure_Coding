bd1dd855e21df86f1dc9ff89b7cf047c9d443513
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-23 19:00:11 -0400

    Optimal Approach using stack<Index>

--- Stack/9. Daily Temperatures.java
+++ Stack/9. Daily Temperatures.java
@@ -5,6 +5,7 @@ import java.util.*;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         
+        /*
         // BrutForce Approach using two for loop
         // TC: O(N^2)
         // SC: O(1)
@@ -42,6 +43,52 @@ class Solution {
                     break;
                 }
             }
+        }
+
+        return res;
+        */
+
+        // -------------------------------------------------------
+
+        // Optimal Approach using stack<Index>
+        // TC: O(N)
+        // SC: O(N) - for using stack
+   
+
+        // *********
+        // Approach
+        /*
+        1. push all the element into stack from back
+        2. if peek Temp <= current temp, 
+                pop untill we get greater element or the stack is empty
+                push that current element index
+        3.  if peek Temp > current,
+                We found our next greater element index
+                distance = peakindex - current index
+                update this into res index
+                push that current element index as well
+        */
+        
+
+        // *********
+        // Code
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[temperatures.length];
+
+        for (int idx = temperatures.length-1; idx >= 0; idx--) {
+            int currentTemp =  temperatures[idx];
+            // removing all that is lower then current
+            while(!st.isEmpty() && temperatures[st.peek()] <= currentTemp) {
+                st.pop();
+            }
+            
+            // here when it will definately find greater element
+            // i.e when peak is greater then current element
+            if(!st.isEmpty()) {
+                res[idx] = st.peek() - idx;
+            }
+
+            st.push(idx);
         }
 
         return res;
