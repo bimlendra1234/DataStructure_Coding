4ba396a0765aee73220bb92b14250bf610969676
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-06-29 15:05:59 -0400

    Optimal Approach using two hashmap

--- Stack/14. Maximum Frequency Stack.java
+++ Stack/14. Maximum Frequency Stack.java
@@ -2,6 +2,7 @@
 
 import java.util.*;
 
+/*
 // BrutForce Approach using stack and HM for frequency count
 // TC push: O(1)
 // SC push: O(1)
@@ -11,13 +12,11 @@ import java.util.*;
 
 // *********
 // Approach
-/*
-Find key with highest frequency
-Traverse stack to find the most recent element with that max frequency
-    push all the element of st to temp and in the process recoginze targetToRemove aswell
-    now reduce the current count as well
-rebuild the original stack
-*/
+// Find key with highest frequency
+// Traverse stack to find the most recent element with that max frequency
+    // push all the element of st to temp and in the process recoginze targetToRemove aswell
+    // now reduce the current count as well
+// rebuild the original stack
 
 
 // *********
@@ -65,6 +64,80 @@ class FreqStack {
             st.push(curr);
         }
         return targetToRemove;
+    }
+}
+*/
+
+// -------------------------------------------------------------
+
+// Optimal Approach using stwo hashmap
+// TC push: O(1)
+// SC push: O(1)
+// TC pop: O(N)
+// SC pop: O(N)
+
+
+// *********
+// Approach
+/*
+Maintain two HM
+    one contain freq
+    other contain freq and its associated stack that has freq number
+In push
+    get the count of val and push into the frequency
+    if that count do not exist in stack create empty stack with that key and insert it
+    if count exist just push into the stack
+    and update maxCount if the value of this count is greater then original maxCount
+In pop
+    find the stack st with max count from stack hashmap
+    pop the value and this will be returned as a result lated
+    once pop reduced the count from freq HM
+    if the st is empty we need to reduce maxCount by 1
+        empty means all the element having max Count is already removed
+        so time to remove now maxCount-1
+
+*/
+
+
+// *********
+// Code
+class FreqStack {
+    Map<Integer, Integer> freq; // val and its count
+    Map<Integer, Stack<Integer>> stack; // freq and its stack containing that freq
+    int maxCount;
+    public FreqStack() {
+        freq = new HashMap<>();
+        stack = new HashMap<>();
+        maxCount = 0;
+    }
+    
+    public void push(int val) {
+        // Update the frequency
+        int valCount = freq.getOrDefault(val,0)+1;
+        freq.put(val, valCount);
+        // put it into the stack
+        if(!stack.containsKey(valCount)) {
+                stack.put(valCount, new Stack<>());
+            }
+            stack.get(valCount).push(val);
+        // update the maxCount
+        if(valCount > maxCount) {
+            maxCount = valCount;
+        }
+    }
+    
+    public int pop() {
+        // get the stack of max frequency
+        Stack<Integer> st = stack.get(maxCount);
+        // remove the value from here
+        int removedValue = st.pop();
+        // reduce t frequency in freq HM as well
+        freq.put(removedValue, freq.get(removedValue)-1);
+        // if st is empty, reduce the maxCount by 1
+        if(st.isEmpty()) {
+            maxCount--;
+        }
+        return removedValue;
     }
 }
 
