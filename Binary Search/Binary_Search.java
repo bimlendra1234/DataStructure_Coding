dd0f0a11ff5c2b3af835d6540fb78b40883a2186
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-02 18:31:44 -0400

    Brut Force Approach without Binary Search

--- Binary Search/Binary_Search.java
+++ Binary Search/Binary_Search.java
@@ -1,17 +1,25 @@
+// Leetcode : 704. Binary Search
+
 class Solution {
     public int search(int[] nums, int target) {
         
-        // Normal Approach without Binary Search
-        
+        // Brut Force Approach without Binary Search
         // TC: O(N)
         // SC: O(1)
 
+
+        // *********
+        // Approach
+        // 1. one traverse and check if it exist
+
+
+        // *********
+        // Code
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == target) {
                 return i;
             }
         }
         return -1;
-
     }
 }
