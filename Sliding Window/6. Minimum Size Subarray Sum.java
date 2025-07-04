31fffc6600e9b71a0c72cc4fd197b299ee32493a
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-04 00:40:18 -0400

    Optimal Approaching using sliding windiw technique

--- Sliding Window/6. Minimum Size Subarray Sum.java
+++ Sliding Window/6. Minimum Size Subarray Sum.java
@@ -3,7 +3,7 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-        // OPtimal Approaching using sliding windiw technique
+        // Optimal Approaching using sliding windiw technique
         // TC: O(N+N) one N for start amd one N for end traversal i.e O(N)
         // SC: O(1)
 
@@ -27,23 +27,22 @@ class Solution {
 
         // *********
         // Code
-        int minSubArrayLen = 0;
-        int start = 0;
+        int minSubArrayLen = 0;  // wont change if no solution
 
+        int start = 0; 
         int sum = 0;
         for(int end = 0; end < nums.length; end++) {
-
             sum += nums[end];
 
             while(sum >= target) {
 
-                if(minSubArrayLen == 0) {
+                if(minSubArrayLen == 0) { // for the first time
                     minSubArrayLen = end-start+1;
                 }
 
-                minSubArrayLen = Math.min(minSubArrayLen, end-start+1);
-                sum -= nums[start];
-                start++;
+                minSubArrayLen = Math.min(minSubArrayLen, end-start+1); // save min len
+                sum -= nums[start]; // remove start from sum -> shrink window 
+                start++; // start++ -> shrink window 
             }
         }
         return minSubArrayLen;
