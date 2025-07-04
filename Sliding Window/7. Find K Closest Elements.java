ffec1bff7c27d83d24abcd5ea5c0de90b23f9e9e
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-04 13:22:45 -0400

    Approach using Binary search and sliding window

--- Sliding Window/7. Find K Closest Elements.java
+++ Sliding Window/7. Find K Closest Elements.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
+        /*
         // Approach using Map not sliding window
         // TC: O(NlogN) due to collection sorting
         // SC: O(N)
@@ -40,6 +40,57 @@ class Solution {
             }
         }
         Collections.sort(res);
+        return res;
+        */
+
+        // ----------------------------------------------------------
+
+        // Approach using Binary search and sliding window
+        // TC: O(log(n−k)+k)
+        // SC: O(1)
+
+
+        // *********
+        // Approach
+        /*
+         1. Use binary search to find the closest element to x
+         2. Use two pointers to find the k closest elements
+         3. left pointer will point to the start of the window
+         4. the right pointer will point to the end of the window not at last 
+              //so that we can have window at last
+         5. The left pointer will be incremented to mid+1
+              // if the mid is closer to x than the element just outside the window
+         6. otherswise The right pointer will be decremented to mid
+         7. The final result will be the elements in the window from left to left+k
+         */
+
+
+        // *********
+        // Code
+
+        // left pointer will point to the start of the window and 
+        // the right pointer will point to the end of the window not at last so that we can have window at last
+        int left = 0;
+        int right = arr.length-k;
+
+        while(left < right) {
+            int mid = (left+right/2);
+
+            //check mid elem and outside window which one is closer to target
+            if(x-arr[mid] > arr[mid+k] -x) {
+                // The left pointer will be incremented to mid+1
+                // if the mid is closer to x than the element just outside the window
+                left = mid+1;
+            } else {
+                // otherswise The right pointer will be decremented to mid
+                right = mid;
+            }
+        }
+        // answer is my window: Left+k
+        List<Integer> res = new ArrayList<>();
+        for(int i = left ; i < left+k; i++) {
+            res.add(arr[i]);
+        }
         return res;
     }
 }
