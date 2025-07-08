f25fe869b5277499a5dbdd5bae90edc7ed912aaa
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-08 15:35:00 -0400

    Approach using without BS but with one traversal

---
+++ 5. Binary Search/8. Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,24 @@
+// Leetcode : 153. Find Minimum in Rotated Sorted Array
+
+class Solution {
+    public int findMin(int[] nums) {
+
+        // Approach using without BS but with one traversal
+        // TC: O(N) - one traversal
+        // SC: O(1)
+
+
+        // *********
+        // Approach
+        // 1. One traversal and finding the minimum number
+
+
+        // *********
+        // Code
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        return min;
+    }
+}
