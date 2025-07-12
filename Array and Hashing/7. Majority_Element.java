ab8423a4d268effe84c8f65f81acf92d5096dc4f
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-12 02:54:30 -0400

    Optimal Approach using Boyer Moore algo

--- Array and Hashing/7. Majority_Element.java
+++ Array and Hashing/7. Majority_Element.java
@@ -1,13 +1,26 @@
+// Leetcode: 169. Majority Element
+
 import java.util.*;
+
 class Solution {
     public int majorityElement(int[] nums) {
         
         /*
         // Brut Force Approach With Non Optimized Hash MAP
-
         // TC - O(N)
         // SC - O(N)
 
+
+        // *********
+        // Approach
+        // 1. Create a HashMap to store the count of each element
+        // 2. Traverse the array and populate the HashMap with counts
+        // 3. find the element with count greater than N/2 in The HashMap
+        // 4. return the key of that element in the hashmap
+
+
+        // *********
+        // Code
         Map<Integer,Integer> HM = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -38,10 +51,12 @@ class Solution {
 
         /*
         // Brut Force Approach With Optimized Hash MAP
-
         // TC - O(N)
         // SC - O(N)
 
+
+        // *********
+        // Code
         Map<Integer,Integer> HM = new HashMap<>();
 
         // get the max count while inserting itself as well
@@ -65,23 +80,32 @@ class Solution {
         // -------------------------------------------------------------------------------------------
 
         
-        // Optimal Approach
-
+        // Optimal Approach using Boyer Moore algo
         // TC - O(N)
         // SC - O(1)
 
+
+        // *********
+        // Approach
         /*
-        // Boyer-Moore Voting Algorithm - only work if there is confirm majority number
-        
-        1. using two variable count = 0 and value = 0
-        2. Do single traversal
-        3. while looking each number
-        4. else on looking each number, if count = 0, change the value to that number and increment the count
-        5. if other number is different just decrement the count by 1 ,
-        6. if the number is same increment the count by 1.
-        6. Finally return the value that is basically the majority number
+        1. maintain majorityElementValue and its count during traversal
+            It denotes what is 
+                majorityElementValue : at specific position
+                count: majorityElement count greater by what number then other at specific position
+        2. do one traversal
+            If element is same as majorityElement value
+                increment the count : it say by what num that majority element is more
+            If element is not same as majorityElement value
+                if count is 0 : this element become the majority now
+                    then update res with that num
+                    and increment its count
+                if count is not zero : already there are some majority element with positive count
+                    so reduce the count
         */
 
+
+        // *********
+        // Code
         int count = 0;
         int majorityElement = 0;
 
