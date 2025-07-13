6b22abe8bce3e9283d98a97e73a8fcb52afe80b2
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-13 15:52:13 -0400

    Approach using sorting and one traversal

--- 6. Linked List/8. Find the Duplicate Number.java
+++ 6. Linked List/8. Find the Duplicate Number.java
@@ -1,8 +1,11 @@
 // Leetcode: 287. Find the Duplicate Number
 
+import java.util.Arrays;
+
 class Solution {
     public int findDuplicate(int[] nums) {
 
+        /*
         // BrutForce approach using two for loop
         // TC: O(N^2)
         // SC: O(1)
@@ -23,6 +26,31 @@ class Solution {
                 if(nums[i] == nums[j]) {
                     res = nums[i];
                 }
+            }
+        }
+        return res;
+        */
+
+        // --------------------------------------------
+
+        // Approach using sorting and one traversal
+        // TC: O(NLOGN) for sorting
+        // SC: O(1)
+
+
+        // *********
+        // Approach 
+        // 1. using sorting and one traversal
+
+
+        // *********
+        // Code
+        Arrays.sort(nums);
+
+        int res = -1;
+        for(int i = 0 ; i < nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                res = nums[i];
             }
         }
         return res;
