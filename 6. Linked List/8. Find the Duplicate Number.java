601b5e27911e5444f1db71836f3b0b335497cc32
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-13 16:51:52 -0400

    Optimal Approach using concept to identify LL cycle and applying fast and slow pointer and the starting point of cycle  is our duplicate

--- 6. Linked List/8. Find the Duplicate Number.java
+++ 6. Linked List/8. Find the Duplicate Number.java
@@ -33,6 +33,7 @@ class Solution {
 
         // --------------------------------------------
 
+        /*
         // Approach using sorting and one traversal
         // TC: O(NLOGN) for sorting
         // SC: O(1)
@@ -54,5 +55,57 @@ class Solution {
             }
         }
         return res;
+        */
+
+        // --------------------------------------------
+
+        // Optimal Approach using concept to identify LL cycle and applying fast and slow pointer
+            // and the starting point of cycle is our duplicate
+        // TC: O(N)
+        // SC: O(1)
+
+
+        // *********
+        // Approach
+        /*
+        1. First find that it is a part of LL cycle
+        eg:     0 1 2 3 4
+               [1,3,4,2,2]
+
+        LL representation and the value where we start cycle is our duplicate
+        0 → 3 → 2 → 4
+                ↑   ↓
+                ←←←←←
+
+        2. Use fast and slow pointer to detect cycle
+        3. when both meet break and 
+            start one new pointer slow2 at start
+                increment both slow and slow2 by 1
+                when they meet that is our duplicate
+                return that number
+        */
+
+
+        // *********
+        // Code
+        int slow = 0;
+        int fast = 0;
+        // since we know that cucle definetly exist
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = 0;
+        while(true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if(slow == slow2) {
+                return slow;
+            }
+        }
     }
 }
