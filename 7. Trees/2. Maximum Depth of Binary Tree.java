e7deed952582a5e465175f9f83424274da1fc437
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-15 16:45:19 -0400

    using BFS traversal

---
+++ 7. Trees/2. Maximum Depth of Binary Tree.java
@@ -0,0 +1,48 @@
+// Leetcode: 104. Maximum Depth of Binary Tree
+
+
+// TC: O(N)
+// SC: O(N)
+
+
+// *********
+// Approach
+/*
+    1. using BFS traversal
+        i am counting each level
+    2. return the level counted
+*/
+
+
+// *********
+// Code
+
+import java.util.*;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+        int level = 0;
+
+        while(!qu.isEmpty()) {
+            level ++;
+            int qSize = qu.size();
+            for(int i = 0; i < qSize; i++) {
+                TreeNode node = qu.poll();
+                if(node.left != null) {
+                    qu.add(node.left);
+                }
+                if(node.right != null) {
+                    qu.add(node.right);
+                }
+            }
+        }
+        return level;
+    }
+}
