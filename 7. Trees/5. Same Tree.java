18035ada2c5ad10333ad288c4ee572ae1a1bbfa2
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-17 12:46:27 -0400

    Both DFS recursion and BFS iterative approach

--- 7. Trees/5. Same Tree.java
+++ 7. Trees/5. Same Tree.java
@@ -1,6 +1,6 @@
 // Leetcode : 100. Same Tree
 
-// Optimal Approach: checking root then left and right subtree
+// DFS recursion Approach: checking root then left and right subtree
 // Time  : O(N + M)  – visit each node once
 // Space : O(max(H, K)) – recursion depth equals taller tree
 
@@ -36,5 +36,68 @@ class Solution {
             return false;
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+// -------------------------------------------------------------
+
+// BFS Iterative Approach: using queue
+// Time  : O(N + M)  – visit each node once
+// Space : O(N + M) in worst case – if both trees are full and completely different shape
+// Space : O(max width of both trees) in typical BFS – i.e., O(min(N, M)) in balanced cases
+
+
+// *********
+// Approach - BFS iterative - level order traversal
+/*
+    1. Use queue DS
+        add root1
+        add root2
+
+    2. run while loop until q is empty 
+        poll first
+        poll second
+
+        // check for first and 2nd equality
+            if both null -> continue and check other
+            if one null and other not || both has different value
+                not same tree
+
+        // Add corresponding children
+            first left child of both
+            then right child of both
+
+    3. if in while loop we didnt find any issue then its a same tree
+*/
+
+
+// *********
+// Code
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(p);
+        qu.add(q);
+
+        while(!qu.isEmpty()) {
+            TreeNode first = qu.poll();
+            TreeNode second = qu.poll();
+
+            // check for 1st and 2nd equality
+            if(first == null && second == null) {
+                continue;
+            }
+            else if(first == null || second == null || first.val != second.val) {
+                return false;
+            }
+
+            // add corresponding children
+            qu.add(first.left);
+            qu.add(second.left);
+            qu.add(first.right);
+            qu.add(second.right);
+        }
+
+        return true;
     }
 }
