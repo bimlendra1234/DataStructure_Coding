5044cac7acfe591de883147f37218bb215888108
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-08-20 13:49:06 -0400

    heap approach with both min heap and max heap

--- 12. Heap or Priority Queue/4. Kth Largest Element in an Array.java
+++ 12. Heap or Priority Queue/4. Kth Largest Element in an Array.java
@@ -52,3 +52,54 @@ class Solution {
 }
 
 // -------------------------------------------------------------------------
+
+// Optimal Approach
+
+// TC: Nlog K
+// SC : O(K)
+
+// *********
+// Approach
+/*
+    1. Use a minHeap (smallest element stays at top).
+
+    2. Insert every num into the heap.
+
+    3. If heap size > k → remove the smallest element.
+        → ensures heap always keeps only k largest numbers.
+
+    4. After processing all elements:
+        - heap contains k largest elements.
+        - the smallest among them (at top) = k-th largest overall.
+
+    5. Return heap.peek().
+*/
+
+/*
+    Time & Space Complexity
+
+    Time:
+        Each offer/poll is O(log k) (heap size never exceeds k).
+        For n elements → O(n log k) total.
+
+    Space:
+        Heap stores at most k elements → O(k).
+*/
+
+// *********
+// Code
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> minHeap  = new PriorityQueue<>();
+
+        for(int num : nums) {
+            minHeap.offer(num);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
