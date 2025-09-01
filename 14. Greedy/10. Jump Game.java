69abfc97c7d8342529ca0b27efd9790b08b2436b
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-09-01 17:38:28 -0400

    Keep track of the farthest index reachable so far (maxIndex)

---
+++ 14. Greedy/10. Jump Game.java
@@ -0,0 +1,35 @@
+// Leetcode : 55. Jump Game
+
+// *********
+// Complexity
+// TC: O(n) → single pass through array
+// SC: O(1) → constant extra space
+
+
+// *********
+// Approach
+/*
+    1. Keep track of the farthest index reachable so far (maxIndex)
+    2. Iterate through array while i ≤ maxIndex
+    3. Update maxIndex = max(maxIndex, i + nums[i])
+    4. If at any point maxIndex ≥ last index → return true
+    5. If loop ends without reaching → return false
+*/
+
+
+// *********
+// Code
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxIndex = 0;
+        int i = 0;
+        while(i <= maxIndex) {
+            maxIndex = Math.max(maxIndex, i+nums[i]);
+            if(maxIndex >= nums.length-1) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
