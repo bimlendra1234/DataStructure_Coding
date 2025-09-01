b09192355879e7395e54db7427ce24afba4a74a1
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-09-01 15:43:14 -0400

    Exactly Same as Fractional Knapsack

---
+++ 14. Greedy/8. Maximum Units on a Truck.java
@@ -0,0 +1,30 @@
+// Leetcode : 1710. Maximum Units on a Truck
+
+
+// Exactly Same as Fractional Knapsack
+
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        
+        // ratio already given as numberOfUnitsPerBoxi
+        // 1. So sort it in decreasing order : greedy as large unit at first
+        Arrays.sort(boxTypes, (a,b) -> b[1]-a[1]);
+
+        int resMaxUnit = 0;
+        for(int[] box: boxTypes) {
+            if(box[0] >= truckSize) { // consume entire truck with max size
+                resMaxUnit += (truckSize * box[1]);
+                truckSize = 0; 
+            }
+            else {
+                resMaxUnit += (box[0] * box[1]);
+                truckSize -= box[0];
+            }
+
+            if(truckSize == 0) {
+                break;
+            }
+        }
+        return resMaxUnit;
+    }
+}
