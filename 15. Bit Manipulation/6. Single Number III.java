eae75d129e8cfade9dee0ad24ac78d1ee8aebf04
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-09-08 00:39:13 -0400

    Optimal Approach using concept ofbuckets and bits manipulation

--- 15. Bit Manipulation/6. Single Number III.java
+++ 15. Bit Manipulation/6. Single Number III.java
@@ -22,3 +22,53 @@ class Solution {
 }
 
 // ----------------------------------------------------
+
+// Optimal Approach using concept ofbuckets and bits manipulation
+
+// *********
+// Complexity
+// TC: O(N) → one pass to XOR + one pass to split into buckets
+// SC: O(1) → only constant extra variables used
+
+
+// *********
+// Approach
+/*
+    1. XOR all numbers → result = xor of the two unique numbers (since duplicates cancel out).
+    2. Find rightmost set bit from xor (different bit between the two unique numbers).
+    3. Partition numbers into two buckets:
+        - If bit set → XOR into bucket1
+        - Else → XOR into bucket2
+    4. Each bucket cancels duplicates inside it → leaving one unique number each.
+    5. Return {bucket1, bucket2}.
+*/
+
+
+// *********
+// Code
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        long xorr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            xorr = xorr ^ nums[i]; // all will be 0 except // xor of two distinct numbers remains
+        }
+
+        long rightMostThatisSet = (xorr & (xorr-1)) ^ xorr;
+        int bucket1 = 0;
+        int bucket2 = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if((rightMostThatisSet & nums[i]) != 0) {
+                bucket1 = bucket1 ^ nums[i];
+            }
+            else {
+                bucket2 = bucket2 ^ nums[i];
+            }
+        }
+
+        return new int[]{bucket1, bucket2};
+
+
+    }
+}
