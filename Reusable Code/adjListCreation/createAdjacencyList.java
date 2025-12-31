8f94a0e3f7969a9bc41d8454bb9d712d55a62b5c
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-12-30 20:33:20 -0500

    added reusabe createadjacency list of graph

---
+++ Reusable Code/adjListCreation/createAdjacencyList.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class createAdjacencyList {
+    
+    public Map<Integer, List<Integer>> isBipartite(int[][] graph){
+            Map<Integer, List<Integer>> adjList = new HashMap<>();
+            int node = graph.length;
+            for(int i = 0; i < node; i++){
+                adjList.put(i, new ArrayList<>());
+            }
+            for(int i = 0; i < node; i++){
+                int u = i;
+                for(int v : graph[i]){
+                    adjList.get(u).add(v);
+                }
+            }
+            return adjList;
+    }
+
+}
+
+// Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
+/*Output: 
+
+    {
+        0=[1, 2, 3], 
+        1=[0, 2], 
+        2=[0, 1, 3], 
+        3=[0, 2]
+    }
+*/
