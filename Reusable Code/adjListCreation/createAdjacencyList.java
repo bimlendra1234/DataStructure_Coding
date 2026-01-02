862fba305c6ce266a3d5a357e39d20a0201907de
Bimlendra Yadav <37120966+bimlendra1234@users.noreply.github.com>
2026-01-02 10:49:53 -0500

    Refactor adjacency list creation methods

--- Reusable Code/adjListCreation/createAdjacencyList.java
+++ Reusable Code/adjListCreation/createAdjacencyList.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class createAdjacencyList {
     
-    public Map<Integer, List<Integer>> createAdjList(int[][] graph){
+    public Map<Integer, List<Integer>> createAdjListFromGraph(int[][] graph){
             Map<Integer, List<Integer>> adjList = new HashMap<>();
             int node = graph.length;
             for(int i = 0; i < node; i++){
@@ -18,14 +18,39 @@ public class createAdjacencyList {
     }
 
 }
-
 // Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
-/*Output: 
-
+/*adjList:
     {
         0=[1, 2, 3], 
         1=[0, 2], 
         2=[0, 1, 3], 
         3=[0, 2]
+    }
+*/
+
+public class createAdjacencyList1 {
+
+    public Map<Integer, List<Integer>> createAdjListFromEdge(int[][] edges, int node){
+            Map<Integer, List<Integer>> adjList = new HashMap<>();
+            for(int i = 0; i < node; i++){
+                adjList.put(i, new ArrayList<>());
+            }
+            for(int[] edge : graph){
+                int u = edge[0];
+                int v = edge[1];
+                adjList.get(u).add(v);
+                adjList.get(v).add(u);
+            }
+            return adjList;
+    }
+
+}
+// Input: node or V = 4, edges[][] = [[0, 1], [0, 2], [1, 2], [2, 3]]
+/*adjList: 
+    {
+        0=[1, 2], 
+        1=[0,2], 
+        2=[0,1,3], 
+        3=[2]
     }
 */
